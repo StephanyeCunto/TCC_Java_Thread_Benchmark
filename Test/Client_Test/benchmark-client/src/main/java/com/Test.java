3c311a6f4e5d77376c54a755c74c0e528359a9f9
StephanyeCunto <stephanyecristine6@gmail.com>
2025-11-12 20:19:07 -0300

    Servidor simples funcionando

---
+++ Test/Client_Test/benchmark-client/src/main/java/com/Test.java
@@ -0,0 +1,50 @@
+package com;
+
+import java.net.URI;
+import java.net.http.*;
+import java.io.IOException; 
+
+
+public class Test {
+    static final String SERVER_URL = "http://localhost:8080";
+    static final int REQUEST_COUNT = 30;
+    static final HttpClient client = HttpClient.newHttpClient();
+
+
+    public void makeRequests(String endPoint){
+        Thread[] threads = new Thread[REQUEST_COUNT];
+
+        for(int i = 0; i < REQUEST_COUNT; i++) {
+            threads[i] = Thread.ofVirtual().start(() -> {
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(SERVER_URL + endPoint)).GET().build();
+                System.out.println( response(request) );
+            });
+        }
+
+        for(int i = 0; i < REQUEST_COUNT; i++) join(threads[i]);
+    } 
+
+    private String response(HttpRequest request){
+        try{
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        }catch(IOException | InterruptedException e){
+            System.out.println(e);
+            return "Erro";
+        }
+    }
+
+    private void join(Thread t){
+        try{
+            t.join();
+        }catch(InterruptedException e){
+            System.out.println(e);
+        }
+    }
+
+    public int handleRequest(){
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(SERVER_URL + "/resetCounter")).DELETE().build();
+        return Integer.parseInt(response(request));
+        
+    };
+} 
