9c6278a66af16133253d6198487b0fe33685afc3
StephanyeCunto <stephanyecristine6@gmail.com>
2025-11-11 22:57:59 -0300

    Criando um projeto spring

---
+++ Teste/benchmark-server/src/test/java/com/benchmark/server/BenchmarkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchmark.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchmarkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
