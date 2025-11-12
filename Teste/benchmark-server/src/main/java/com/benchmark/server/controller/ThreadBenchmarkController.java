bfa4de268bb3c0d51d14946d9446266ab5102e31
StephanyeCunto <stephanyecristine6@gmail.com>
2025-11-12 00:26:24 -0300

    Servidor criado

---
+++ Teste/benchmark-server/src/main/java/com/benchmark/server/controller/ThreadBenchmarkController.java
@@ -0,0 +1,57 @@
+package com.benchmark.server.controller;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ThreadBenchmarkController {
+    private static final long SLEEP_DURATION_MS = 1000;
+    private final AtomicInteger threadCounter = new AtomicInteger(0);
+
+    @GetMapping("/virtualThread")
+    public String startVirtualThread() {
+        Thread.ofVirtual().start(() -> simulateWork());
+        threadCounter.incrementAndGet();
+
+        return "Thread virtual iniciada! Veja o console do servidor.";
+    }
+
+    @GetMapping("/traditionalThread")
+    public String startTraditionalThread() {
+        Thread t = new Thread(() -> simulateWork());
+
+        t.start();
+
+        try{
+            t.join();
+        }catch(InterruptedException e){
+            System.out.println(e);
+        }
+        threadCounter.incrementAndGet();
+
+        return "Thread iniciada! Veja o console do servidor.";
+    }
+
+    private void simulateWork(){
+        try{
+            Thread.sleep(SLEEP_DURATION_MS);
+        }catch(InterruptedException e){
+            System.out.println(e);
+        }
+    }
+
+    @GetMapping("/getCounter")
+    public int getCounter() {
+        return threadCounter.get();
+    }
+
+   @DeleteMapping("/resetCounter")
+    public int resetCounter() {
+        int oldValue = threadCounter.getAndSet(0); 
+        return oldValue;
+    }
+}
+
