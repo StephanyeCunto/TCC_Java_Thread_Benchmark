72d300e04f7a3bfb50d450f7b92eb01d3f76df79
StephanyeCunto <stephanyecristine6@gmail.com>
2025-11-12 22:40:24 -0300

    Pequenas alterações

--- Test/Client_Test/benchmark-client/src/main/java/com/Main.java
+++ Test/Client_Test/benchmark-client/src/main/java/com/Main.java
@@ -1,12 +1,25 @@
 package com;
 
-public class Main {
-    public static void main(String[] args) {
-        Test t = new Test();
-        long startTime = System.currentTimeMillis();
-        t.makeRequests("/traditionalThread");
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.*;
+import java.util.concurrent.*;
 
-        long endTime = System.currentTimeMillis() - startTime;
-        System.out.println("Requisições que chegaram ao servidor: "+t.handleRequest() + "\n Tempo: "+ endTime);
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Options opt = new OptionsBuilder().include(Main.class.getSimpleName()).warmupIterations(0).measurementIterations(3).build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.SingleShotTime,Mode.AverageTime,Mode.Throughput,Mode.SampleTime})
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Fork(1)
+    public void makeRequests(MakeRequestsState state, Blackhole blackhole) {
+        state.getTest().makeRequests(state.getEndPoint());
+       // System.out.println(state.getTest().handleRequest());
     }
 }
