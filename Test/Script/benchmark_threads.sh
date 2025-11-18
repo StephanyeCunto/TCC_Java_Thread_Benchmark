52403e501882c318c3f213c583609361c7a83925
StephanyeCunto <stephanyecristine6@gmail.com>
2025-11-18 09:39:39 -0300

    Código funcionando

--- Test/Script/benchmark_threads.sh
+++ Test/Script/benchmark_threads.sh
@@ -4,20 +4,11 @@
 
 BASE_URL="$1"
 
-# Usuário e IP da VM + chave privada
 SERVER="azureuser@20.195.171.67"
 KEY_PATH="$HOME/.ssh/linux-java-vm_key.pem"
 JFR_PATH="/home/azureuser/jfr"
 JAVA_JAR_PATH="/home/azureuser/TCC_Java_Thread_Benchmark/Test/Serve_Test/benchmark-server/target/benchmark-server-0.0.1-SNAPSHOT.jar"
 
-# Ajustes de limites (macOS pode não permitir alguns)
-ulimit -n 20000
-ulimit -s 20000
-ulimit -v unlimited
-
-#############################################
-# Função para iniciar JFR remoto
-#############################################
 
 close_port() {
     result=$(ssh -i "$KEY_PATH" "$SERVER" "lsof -t -i :8080")
@@ -46,50 +37,49 @@ start_jfr() {
     sleep 5
 }
 
-# Função para parar JFR remoto
 stop_jfr() {
     ssh -i "$KEY_PATH" "$SERVER" "kill \$(cat $JFR_PATH/server.pid); echo 'JFR parado'"
 }
 
-# Função para baixar o JFR
 download_jfr() {
     NAME="$1"
     scp -i "$KEY_PATH" "$SERVER:$JFR_PATH/$NAME" "results/$NAME"
 }
 
-for j in {1..1}; do
-    if [ $(($j % 2)) -eq 0 ]; then
-        ENDPOINT="threads/virtual"
-    else
-        ENDPOINT="threads/traditional"
-    fi
-    #############################################
-    # WARM-UP
-    #############################################
-    start_jfr "${ENDPOINT}${j}.jfr"
+warmup(){
+    ENDPOINT="$1"
+    j="$2"
     echo "=== Warm-up ==="
-    echo "GET $BASE_URL/$ENDPOINT" | vegeta attack -duration=60s -rate=300 \
+    echo "GET $BASE_URL/$ENDPOINT" | vegeta attack -duration=1s -rate=300 \
         | tee "results/${ENDPOINT}/${j}/warmup.bin" \
         | vegeta report --type=json > "results/${ENDPOINT}/${j}/warmup.json"
+
+    saveGet "results/${ENDPOINT}/${j}/warmupGet.json"
+
     curl -s "Get $BASE_URL/gc"
     sleep 20
+}
 
-    # ############################################
-    # PRÉ-CARGA 1000 req/s
-    # ############################################
+load(){
+    ENDPOINT="$1"
+    j="$2"
     for i in 1 2; do
         echo "=== Pré-carga 1000 req/s - $i ==="
         echo "GET $BASE_URL/$ENDPOINT" | vegeta attack -duration=60s -rate=1000 \
             | tee "results/${ENDPOINT}/${j}/preload_${i}.bin" \
             | vegeta report --type=json > "results/${ENDPOINT}/${j}/preload_${i}.json"
+
+        saveGet "results/${ENDPOINT}/${j}/preload_${i}Get.json"
+        
         curl -s "Get $BASE_URL/gc"
         sleep 20
     done
+}
 
-    # ############################################
-    # LOOP PRINCIPAL
-    # ############################################
-    for i in {1..150}; do
+loop(){
+    ENDPOINT="$1"
+    j="$2"
+    for i in {1..20}; do
         RATE=$((50 * i))
         JFR_NAME="run_${RATE}.jfr"
 
@@ -103,9 +93,38 @@ for j in {1..1}; do
             | tee "results/${ENDPOINT}/${j}/run_${RATE}.bin" \
             | vegeta report --type=json > "results/${ENDPOINT}/${j}/run_${RATE}.json"
         sleep 60
+
+        saveGet "results/${ENDPOINT}/${j}/run_${RATE}Get.json"
+
         curl -s "Get $BASE_URL/gc"
         sleep 20
     done
+
+}
+
+saveGet(){
+    ADRRES="$1"
+    sleep 20
+    echo $ADRRES
+        echo "GET $BASE_URL/get"| vegeta attack -duration=1s -rate=1 \
+        | vegeta report --type=json > $ADRRES
+}
+
+for j in {1..10}; do
+    if [ $(($j % 2)) -eq 0 ]; then
+        ENDPOINT="threads/virtual"
+    else
+        ENDPOINT="threads/traditional"
+    fi
+    
+    start_jfr "${ENDPOINT}${j}.jfr"
+
+    warmup ${ENDPOINT} ${j}
+
+    load ${ENDPOINT} ${j}
+
+    loop ${ENDPOINT} ${j}
+
     stop_jfr   
     download_jfr "${ENDPOINT}${j}.jfr"
 
