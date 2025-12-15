49480482f9bef70d9608b3f94ba86eceefb0f03e
StephanyeCunto <stephanyecristine6@gmail.com>
2025-12-15 06:52:08 -0300

    Subindo codigo

--- Teste/Script/loadRamping/benchmark_threads.sh
+++ Teste/Script/loadRamping/benchmark_threads.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
-# ./benchmark_threads.sh "http://20.195.171.67:8080" 
+# ./benchmark_threads.sh "20.195.171.67"
 
 BASE_URL="http://$1:8080/threads"
 SSH="ssh stephanye@$1"
 
 JAVA_JAR_PATH="documents/tcc_teste/Test/Serve_Test/benchmark-server/target/benchmark-server-0.0.1-SNAPSHOT.jar"
-LOG_PATH="documents/tcc_teste/Test/Script/LoadConstant/Results/logs"
+LOG_PATH="documents/tcc_teste/Test/Script/LoadRamping/Results/logs"
 RESULTS_PATH="Results/results"
+
+prepare_environment() {
+    echo "=== Preparando ambiente no servidor (macOS Apple Silicon) ==="
+
+    SENHA_SUDO="120504"
+
+    $SSH "
+        echo '>> ulimit (max files)'
+        ulimit -n 1048576 || true
+
+        echo '>> sysctl macOS (files & network)'
+        echo '$SENHA_SUDO' | sudo -S sysctl -w kern.maxfiles=1048576
+        echo '$SENHA_SUDO' | sudo -S sysctl -w kern.maxfilesperproc=1048576
+
+        echo '>> sysctl macOS (TCP buffers)'
+        echo '$SENHA_SUDO' | sudo -S sysctl -w net.inet.tcp.sendspace=2097152
+        echo '$SENHA_SUDO' | sudo -S sysctl -w net.inet.tcp.recvspace=2097152
+        echo '$SENHA_SUDO' | sudo -S sysctl -w net.inet.tcp.msl=1000
+        echo '$SENHA_SUDO' | sudo -S sysctl -w net.inet.tcp.delayed_ack=0
+
+        echo '>> sysctl macOS (EPHEMERAL PORT RANGE)'
+        echo '$SENHA_SUDO' | sudo -S sysctl -w net.inet.ip.portrange.first=1024
+        echo '$SENHA_SUDO' | sudo -S sysctl -w net.inet.ip.portrange.last=65535
+
+        echo '>> Conferência portas efêmeras'
+        sysctl net.inet.ip.portrange.first
+        sysctl net.inet.ip.portrange.last
+
+        echo '>> Informações do sistema'
+        uname -a
+        sysctl -n hw.ncpu
+        sysctl -n hw.memsize | awk '{print \$1/1024/1024/1024 \" GB\"}'
+        ulimit -n
+    "
+}
 
 close_port() {
     result=$($SSH "lsof -t -i :8080")
@@ -27,13 +62,14 @@ start_jvm() {
     j="$2"
 
     close_port
+    prepare_environment
 
     $SSH "
         mkdir -p $LOG_PATH/$ENDPOINT
         nohup java -jar $JAVA_JAR_PATH > $LOG_PATH/$ENDPOINT/java${j}.log 2>&1 &
         echo \$! > $LOG_PATH/server.pid
     "
- 
+
     echo 'jvm iniciado'
     sleep 10
 }
@@ -58,20 +94,20 @@ warmup(){
 loop(){
     ENDPOINT="$1"
     j="$2"
+
     for i in {1..20}; do
         RATE=$((50 * i))
-
         echo "=== Teste $RATE req/s ==="
 
         echo "GET $BASE_URL/$ENDPOINT" | vegeta attack \
-        -duration="10s" \
-        -rate="$RATE" \
-        -timeout=0s \
-        | tee "$RESULTS_PATH/$ENDPOINT/$j/run/bin/run${RATE}.bin" \
-        | vegeta report --type=json > "$RESULTS_PATH/$ENDPOINT/$j/run/json/run${RATE}.json"
+            -duration="10s" \
+            -rate="$RATE" \
+            -timeout=0s \
+            | tee "$RESULTS_PATH/$ENDPOINT/$j/run/bin/run${RATE}.bin" \
+            | vegeta report --type=json > "$RESULTS_PATH/$ENDPOINT/$j/run/json/run${RATE}.json"
 
-    gc
-    sleep 60
+        gc
+        sleep 60
     done
 }
 
@@ -121,18 +157,18 @@ for j in {1..10}; do
     else
         ENDPOINT="traditional"
     fi
-    
-    create_folders "${ENDPOINT}" "${j}"
-    
-    start_jvm ${ENDPOINT} ${j}
 
-    warmup ${ENDPOINT} ${j}
+    create_folders "${ENDPOINT}" "${j}"
+
+    start_jvm "${ENDPOINT}" "${j}"
+
+    warmup "${ENDPOINT}" "${j}"
 
     gc
 
     loadMonitor "${ENDPOINT}" "${j}"
 
-    loop ${ENDPOINT} ${j}
+    loop "${ENDPOINT}" "${j}"
 
     stop_jvm 
 
