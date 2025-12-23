228c28a30ccc02e346c007aa53fb203e2092d509
StephanyeCunto <stephanyecristine6@gmail.com>
2025-12-23 07:21:23 -0300

    Subindo alteracoes

--- Teste/Script/loadRamping/benchmark_threads.sh
+++ Teste/Script/loadRamping/benchmark_threads.sh
@@ -11,8 +11,8 @@ SSH="ssh stephanye@$1"
 SERVER_DIR="Documents/tcc/Teste/Script/LoadRamping"
 JAVA_JAR_ADRESS="Documents/tcc/Teste/Serve_Test/benchmark-server/target"
 JAVA_JAR_PATH="$JAVA_JAR_ADRESS/benchmark-server-0.0.1-SNAPSHOT.jar"
-LOG_PATH="$SERVER_DIR/Results/logs"
-RESULTS_PATH="Results/"
+LOG_PATH="$SERVER_DIR/Results2/logs"
+RESULTS_PATH="Results2/"
 
 source "$ROOT_DIR/prepare_environment.sh"
 source "$ROOT_DIR/jvm.sh"
@@ -107,7 +107,7 @@ for j in {1..10}; do
     name=Monitor duration=60000s settings=$JAVA_JAR_ADRESS/teste.jfc jdk.CPULoad#period=1s jdk.PhysicalMemory#period=1s \
     filename=$SERVER_DIR/$RESULTS_PATH/$ENDPOINT/$j/Monitor/Monitor.jfr
     "
-    
+
     loop "${ENDPOINT}" "${j}"
 
     $SSH 'jcmd $(cat '"$LOG_PATH"'/server.pid) JFR.stop name='"Monitor"' '
